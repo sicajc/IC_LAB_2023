18dad0d0408f6281a3607fad79d618b00b2800d7
sicajc <jackyyeh1999@gmail.com>
2023-10-23 11:34:27 +0800

    Final version

---
+++ lab05/performance.py
@@ -0,0 +1,18 @@
+
+area0 = 1404239.510787
+cycle0 = 10
+performance0 = (area0**2) * cycle0
+
+print(performance0)
+
+area1 = 1394000.510787
+cycle1 = 11
+performance1 = (area1**2) * cycle1
+
+print(performance1)
+
+area0 = 1442052
+cycle0 = 7.5
+performance0 = (area0**2) * cycle0
+
+print(performance0)
